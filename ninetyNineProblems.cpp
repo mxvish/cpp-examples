3c23896aab13d390bf87058cd239ad28d44d0630
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-05-24 19:01:01 +0900

    Add files via upload

---
+++ ninetyNineProblems.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void p001(vector<string> list){
+  cout << list[list.size()-1] + "\n";
+}
+
+int main(){
+  p001({"a", "b", "c", "d"});
+}
+//https://www.ic.unicamp.br/~meidanis/courses/mc336/problemas-lisp/L-99_Ninety-Nine_Lisp_Problems.html
